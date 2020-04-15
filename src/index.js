86e01da75ed0bbcf80d60e3fc76ac288961f7eb3
Serhij Korneluk <serhij@debian.home>
2020-04-15 08:22:08 +0300

    The Square component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Square from './components/Square';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Square />,
   document.getElementById('root')
 );
