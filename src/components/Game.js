165d86bf56423f46b6d6d7640639e876c02ac34a
Serhij Korneluk <serhij@debian.home>
2020-04-15 08:40:37 +0300

    refactoring the Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,24 +2,31 @@ import React from 'react';
 import Square from './Square';
 
 function Game() {
+  function renderSquare(i) {
+    return <Square
+      value={i}
+      onClick={null}
+    />;
+  }
+
   return (
     <div className="container">
       <div className="game">
         <div className="game-board">
           <div className="board-row">
-            <Square value={0} onClick={null}/>
-            <Square value={1} onClick={null}/>
-            <Square value={2} onClick={null}/>
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
           </div>
           <div className="board-row">
-            <Square value={3} onClick={null}/>
-            <Square value={4} onClick={null}/>
-            <Square value={5} onClick={null}/>
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
           </div>
           <div className="board-row">
-            <Square value={6} onClick={null}/>
-            <Square value={7} onClick={null}/>
-            <Square value={8} onClick={null}/>
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
           </div>
         </div>
       </div>
