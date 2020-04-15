9d23c1690068f56fd7edeebb813734efe61a8daf
Serhij Korneluk <serhij@debian.home>
2020-04-15 08:31:58 +0300

    The Game component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Square from './components/Square';
+import Game from './components/Game';
 
 ReactDOM.render(
-  <Square />,
+  <Game />,
   document.getElementById('root')
 );
